900dd3fde6525d2114ed738bb5fc18f00b67f7f7
Bernhard Stoeffler <bernhard.stoeffler@tum.de>
2022-12-13 12:16:01 +0100

    added cronjob

--- actors.py
+++ actors.py
@@ -11,11 +11,12 @@ class actor:
 	location = "nowhere"
 	type = "nothing"
 	name = "nobody"
+	topic = "actor/undefined"
 	# execute actions by sending a mqtt message to the topic
  	# "actor/location/type/name/command"
 
 	def __init__(self):
-		self.topic = "actor/" + self.location + "/" + self.type + "/" + self.name
+		# self.topic = "actor/" + self.location + "/" + self.type + "/" + self.name
 		self.client = mqtt_client.Client(self.type + "_" + self.name)
 		self.client.on_message = self.on_message
 		try:
@@ -41,6 +42,10 @@ class actor:
 	def publish_state(self, topic, payload):
 		self.client.publish(topic, payload, qos=1, retain=True)
 
+	# publish a mqtt message to another actor
+	def publish_command(self, topic, payload):
+		self.client.publish(topic, payload)
+
 	# run a command which is requested via mqtt
 	def invoke_action(self, topic, payload):
 		commandstring = topic.replace(self.topic + "/", '', 1)
@@ -59,7 +64,7 @@ class actor:
 	def update_states(self, topic, payload):
 		self.states_internal[topic] = payload
 
-	# update multiple states at once
+	# update multiple states at once, given a dict where the keys are topics
 	def publish_multiple_states(self, dict):
 		for key in dict.keys():
 			self.publish_state(key, dict[key])
@@ -80,42 +85,73 @@ class actor:
 
 ##################################################
 
+class cronjob(actor):
+	type = "cronjob"
+	location = "internal"
+	name = "cronjob" 
+	topic = "actor/cronjob"
+
+	states_internal = {
+		"state/time/hour": 0,
+		"state/time/minute": 0,
+		"state/time/daytime": "day",
+		"state/bernie/light/mode": "off" # to switch between day and evening
+	}
+
+	# called every minute
+	def minute(self, payload):
+		self.publish_state("state/time/minute", payload)
+
+	# called every hour
+	def hour(self, payload):
+		self.publish_state("state/time/hour", payload)
+		# adjust daytime
+		if int(payload) == 7:
+			self.daytime("day")
+		elif int(payload) == 20:
+			self.daytime("evening")
+		elif int(payload) == 1:
+			self.daytime("night")
+
+	# called when daytime is changing
+	def daytime(self, payload):
+		self.publish_state("state/time/daytime", payload)
+		# switch to a nicer lighting in the evening
+		if self.states_internal["state/bernie/light/mode"] == "on" \
+			and payload != "day":
+			self.publish_state("state/bernie/light/mode", "mood")
+		elif self.states_internal["state/bernie/light/mode"] == "mood" \
+			and payload == "day":
+			self.publish_state("state/bernie/light/mode", "on")
+
+	actions = {
+		"minute": minute,
+		"hour": hour,
+		"daytime": daytime
+	}
+
+
+
+
 class lightswitch_bernie_shelly(actor):
 	type = "lightswitch"
 	location = "bernie"
 	name = "shelly"
+	topic = "actor/bernie/lightswitch/shelly"
 
 	states_internal = {
-		"state/time/hour": 0,
-		"state/bernie/light/mode": 0
-	}
-	states_on_day = {
-		"state/bernie/light/mode": 1,
-		"state/bernie/light/ceiling/power": 1,
-		"state/bernie/light/floor/power": 0,
-		"state/bernie/starprojector/power": 0
-	}
-	states_on_evening = {
-		"state/bernie/light/mode": 2, 
-		"state/bernie/light/ceiling/power": 0,
-		"state/bernie/light/floor/power": 1,
-		"state/bernie/starprojector/power": 1
-	}
-	states_off = {
-		"state/bernie/light/mode": 0,
-		"state/bernie/light/ceiling/power": 0,
-		"state/bernie/light/floor/power": 0,
-		"state/bernie/starprojector/power": 0
+		"state/time/daytime": "day",
+		"state/bernie/light/mode": "off"
 	}
 
 	def turn_on(self, payload):
-		if int(self.states_internal["state/time/hour"]) < 20:
-			self.publish_multiple_states(self.states_on_day)
+		if self.states_internal["state/time/daytime"] == "day":
+			self.publish_state("state/bernie/light/mode", "on")
 		else:
-			self.publish_multiple_states(self.states_on_evening)
+			self.publish_state("state/bernie/light/mode", "mood")
 
 	def turn_off(self, payload):
-		self.publish_multiple_states(self.states_off)
+		self.publish_state("state/bernie/light/mode", "off")
 
 	def toggle(self, payload):
 		if int(self.states_internal["state/bernie/light/mode"]) > 0:
@@ -129,5 +165,6 @@ class lightswitch_bernie_shelly(actor):
 		"toggle": toggle
 	}
 
-
+# maybe move instances to main mqtthandler.py
 instance_lightswitch_zimmer = lightswitch_bernie_shelly()
+instance_cronjob = cronjob()
