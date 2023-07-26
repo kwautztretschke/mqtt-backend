7b528b9cb50406c710ba8888b7e464ae8db9c363
Bernie Stoeffler <bernie.stoeffler@tum.de>
2023-07-26 09:35:34 +0200

    fixed fucking indentation god I love python

--- baseclasses.py
+++ baseclasses.py
@@ -5,7 +5,7 @@ port = 1883
 
 
 class mqtt_actor:
-    ############################### variables to be redefined by derived classes
+	############################### variables to be redefined by derived classes
 	# information about the reactor
 	location = "nowhere"
 	type = "nothing"
@@ -88,63 +88,63 @@ class mqtt_actor:
 		print("Hello World, payload: " + payload)
 
 
- 
- class mqtt_reactor:
-    ############################### variables to be redefined by derived classes
+
+class mqtt_reactor:
+	############################### variables to be redefined by derived classes
 	# information about the reactor
-    location = "nowhere"
-    type = "nothing"
-    name = "nobody"
+	location = "nowhere"
+	type = "nothing"
+	name = "nobody"
 	topic = "reactor/undefined"
 
-    # a dict containing states' values, to be used in logic, which gets updated
-    states = {
-        "foo": 0,
-        "bar": 0
-    }
+	# a dict containing states' values, to be used in logic, which gets updated
+	states = {
+		"foo": 0,
+		"bar": 0
+	}
 	
 	# a dict containing the states that should just be passed through to the reactor
 	passthrough_states = {
-        # the payload for state/foo will be sent to reactor/undefined/wallah
-        "foo": "wallah",
-    }
- 
+		# the payload for state/foo will be sent to reactor/undefined/wallah
+		"foo": "wallah",
+	}
+
 	# a dict containing presets and the respective set of commands
 	presets = {
-        "A": [("wallah", "42")],
-        "B": [
-            ("wallah", "187"),
-            ("billah", "207")
-        ],
-    }
+		"A": [("wallah", "42")],
+		"B": [
+			("wallah", "187"),
+			("billah", "207")
+		],
+	}
 
 	################################ general mqtt related utilities
 	# constructor, initializes the mqtt client
-    def __init__(self):
-        self.client = mqtt_client.Client(self.type + "_" + self.name)
-        self.client.on_message = self.on_message
-        try:
-            self.client.connect(broker, port)
-            self.subscribe_states()
-            print(f"Reactor {self.name} connected successfully")
-            self.client.loop_start()
-        except:
-            print("Connection error!")
+	def __init__(self):
+		self.client = mqtt_client.Client(self.type + "_" + self.name)
+		self.client.on_message = self.on_message
+		try:
+			self.client.connect(broker, port)
+			self.subscribe_states()
+			print(f"Reactor {self.name} connected successfully")
+			self.client.loop_start()
+		except:
+			print("Connection error!")
 
-    # callback to be executed by mqtt client on message received
+	# callback to be executed by mqtt client on message received
 	def on_message(self, client, userdata, msg):
 		self.update_states(msg.topic, msg.payload.decode())
 
 		if not self.passthrough_state(msg.topic, msg.payload.decode()):
 			self.determine_preset()  # Call the derived class's determine_preset function
 
-    # subscribe the mqtt client to all topics in the states dict
-    def subscribe_states(self):
-        for key in self.states.keys():
-            self.client.subscribe("state/" + key)
+	# subscribe the mqtt client to all topics in the states dict
+	def subscribe_states(self):
+		for key in self.states.keys():
+			self.client.subscribe("state/" + key)
 
-    # update the internal variable (dict entry) if a state changes
-    def update_state(self, topic, payload):
+	# update the internal variable (dict entry) if a state changes
+	def update_state(self, topic, payload):
 		state = topic.replace("state/", '', 1)
 		self.states[state] = payload
 
@@ -156,18 +156,18 @@ class mqtt_actor:
 	# Check if the received message matches any of the passthrough topics
 	def passthrough_state(self, topic, payload):
 		state = topic.replace("state/", '', 1)
-        passthrough = self.passthrough_states.get(state)
-        if passthrough:
-            self.publish_to_reactor(passthrough, payload, True)
-            return True  # Return True if the state was found in the passthrough dict
-        return False  # Return False if not
+		passthrough = self.passthrough_states.get(state)
+		if passthrough:
+			self.publish_to_reactor(passthrough, payload, True)
+			return True  # Return True if the state was found in the passthrough dict
+		return False  # Return False if not
 
 	# depending on the states, apply a preset of commands
-    def determine_preset(self):
-        pass  # Derived classes will implement this method to determine and apply presets
+	def determine_preset(self):
+		pass  # Derived classes will implement this method to determine and apply presets
 
 	# Apply the preset by sending the corresponding topic/payload tuples to the hardware
-    def apply_preset(self, preset_name):
-        for command, payload in self.presets[preset_name]:
-            self.publish_to_reactor(topic, payload, True)
+	def apply_preset(self, preset_name):
+		for command, payload in self.presets[preset_name]:
+			self.publish_to_reactor(topic, payload, True)
 
--- mqtt_reactors.py
+++ mqtt_reactors.py
@@ -1,56 +1,56 @@
 from baseclasses import mqtt_reactor as reactor
 
 class bedroom_whiteboard(reactor):
-    type = "monochrome_led_strip"
-    location = "bedroom"
-    name = "whiteboard"
-    topic = "reactor/bedroom/whiteboard"
+	type = "monochrome_led_strip"
+	location = "bedroom"
+	name = "whiteboard"
+	topic = "reactor/bedroom/whiteboard"
 
 	states = {
 		"bedroom/light/mode": "off",
 		"bedroom/music": "off",
 	}
- 
-    presets = {
-        "off": [
-            ("power", "off")
+
+	presets = {
+		"off": [
+			("power", "off")
 		],
-        "daytime_light": [
-            ("power", "on"),
-            ("program", "solidColor"),
-            ("color", "00FFFF"),
-            ("brightness", "128")
-        ],
-        "mood_light": [
-            ("power", "on"),
-            ("program", "solidColor"),
-            ("color", "FF0000"),
-            ("brightness", "64")
-        ],
-        "silent_party": [
-            ("power", "on"),
-            ("program", "solidColor"),
-            ("color", "00FF00"),
-            ("brightness", "255")
-        ],
-        "music_party": [
-            ("power", "on"),
-            ("program", "syncedColor"),
-            ("color", "00FF00"),
-            ("brightness", "255")
-        ]
-    }
+		"daytime_light": [
+			("power", "on"),
+			("program", "solidColor"),
+			("color", "00FFFF"),
+			("brightness", "128")
+		],
+		"mood_light": [
+			("power", "on"),
+			("program", "solidColor"),
+			("color", "FF0000"),
+			("brightness", "64")
+		],
+		"silent_party": [
+			("power", "on"),
+			("program", "solidColor"),
+			("color", "00FF00"),
+			("brightness", "255")
+		],
+		"music_party": [
+			("power", "on"),
+			("program", "syncedColor"),
+			("color", "00FF00"),
+			("brightness", "255")
+		]
+	}
 
 	passthrough_states = {
-        "bedroom/light/brightness": "brightness",
-    }
+		"bedroom/light/brightness": "brightness",
+	}
 
-    def determine_preset(self):
-        # Implement the logic to determine and apply the appropriate preset based on the states
-        if self.states["bedroom/light/mode"] == "off":
-            self.apply_preset("off")
-        elif self.states["bedroom/light/mode"] == "on":
-            self.apply_preset("daytime_light")
+	def determine_preset(self):
+		# Implement the logic to determine and apply the appropriate preset based on the states
+		if self.states["bedroom/light/mode"] == "off":
+			self.apply_preset("off")
+		elif self.states["bedroom/light/mode"] == "on":
+			self.apply_preset("daytime_light")
 		elif self.states["bedroom/light/mode"] == "mood":
 			self.apply_preset("mood_light")
 		elif self.states["bedroom/light/mode"] == "party":
