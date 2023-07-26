dee714b51a45088c88d9519749a67e20beb532a0
Bernie Stoeffler <bernie.stoeffler@tum.de>
2023-07-26 09:03:36 +0200

    fixed some variables

--- mqtt_actors.py
+++ mqtt_actors.py
@@ -10,10 +10,10 @@ class cronjob(actor):
 	invoke_action_verbose = False
 
 	states = {
-		"state/time/hour": 0,
-		"state/time/minute": 0,
-		"state/time/daytime": "day",
-		"state/bedroom/light/mode": "off" # to switch between day and evening
+		"time/hour": 0,
+		"time/minute": 0,
+		"time/daytime": "day",
+		"bedroom/light/mode": "off" # to switch between day and evening
 	}
 
 	commands = {
@@ -24,11 +24,11 @@ class cronjob(actor):
 
 	# called every minute
 	def minute(self, payload):
-		self.publish_state("state/time/minute", payload)
+		self.publish_state("time/minute", payload)
 
 	# called every hour
 	def hour(self, payload):
-		self.publish_state("state/time/hour", payload)
+		self.publish_state("time/hour", payload)
 		# adjust daytime
 		if int(payload) == 7:
 			self.daytime("day")
@@ -39,25 +39,26 @@ class cronjob(actor):
 
 	# called when daytime is changing
 	def daytime(self, payload):
-		self.publish_state("state/time/daytime", payload)
+		self.publish_state("time/daytime", payload)
 		# switch to a nicer lighting in the evening
-		if self.states["state/bedroom/light/mode"] == "on" \
+		if self.states["bedroom/light/mode"] == "on" \
 			and payload != "day":
-			self.publish_state("state/bedroom/light/mode", "mood")
-		elif self.states["state/bedroom/light/mode"] == "mood" \
+			self.publish_state("bedroom/light/mode", "mood")
+		# or a brighter one at daytime
+		elif self.states["bedroom/light/mode"] == "mood" \
 			and payload == "day":
-			self.publish_state("state/bedroom/light/mode", "on")
+			self.publish_state("bedroom/light/mode", "on")
 
 
-class lightswitch_bedroom_shelly(actor):
-	type = "lightswitch"
+class lightswitch_bedroom(actor):
+	type = "shelly"
 	location = "bedroom"
-	name = "shelly"
-	topic = "actor/bedroom/lightswitch/shelly"
+	name = "lightswitch"
+	topic = "actor/bedroom/lightswitch"
 
 	states = {
-		"state/time/daytime": "day",
-		"state/bedroom/light/mode": "off"
+		"time/daytime": "day",
+		"bedroom/light/mode": "off"
 	}
 
 	commands = {
@@ -67,16 +68,16 @@ class lightswitch_bedroom_shelly(actor):
 	}
 
 	def turn_on(self, payload):
-		if self.states["state/time/daytime"] == "day":
-			self.publish_state("state/bedroom/light/mode", "on")
+		if self.states["time/daytime"] == "day":
+			self.publish_state("bedroom/light/mode", "on")
 		else:
-			self.publish_state("state/bedroom/light/mode", "mood")
+			self.publish_state("bedroom/light/mode", "mood")
 
 	def turn_off(self, payload):
-		self.publish_state("state/bedroom/light/mode", "off")
+		self.publish_state("bedroom/light/mode", "off")
 
 	def toggle(self, payload):
-		if self.states["state/bedroom/light/mode"] != "off":
+		if self.states["bedroom/light/mode"] != "off":
 			self.turn_off('')
 		else:
 			self.turn_on('')
